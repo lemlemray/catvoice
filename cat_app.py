6768695ecf154cca3f1de6efed11508862e2cc52
しましま <j.d.i.k.4110@gmail.com>
2026-03-08 01:55:29 +0900

    Add files via upload

--- cat_app.py
+++ cat_app.py
@@ -1,38 +1,17 @@
-import streamlit as st
+from pydub import AudioSegment
 import librosa
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+import tempfile
 
-st.title("🐈 猫翻訳AI")
+uploaded_file = st.file_uploader("猫の声をアップロード", type=["wav","mp3","m4a","mp4","mov"])
 
-uploaded_file = st.file_uploader(
-    "猫の鳴き声をアップロードしてください",
-    type=["wav","mp3","m4a","mp4"]
-)
+if uploaded_file:
 
-if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(uploaded_file.read())
+        tmp_path = tmp.name
 
-    y, sr = librosa.load(uploaded_file)
+    audio = AudioSegment.from_file(tmp_path)
+    wav_path = tmp_path + ".wav"
+    audio.export(wav_path, format="wav")
 
-    mfcc = librosa.feature.mfcc(y=y, sr=sr)
-    feature = np.mean(mfcc.T, axis=0)
-
-    X = [
-        feature,
-        feature * 0.9,
-        feature * 1.1
-    ]
-
-    y_labels = [
-        "甘え",
-        "ご飯要求",
-        "怒り"
-    ]
-
-    model = RandomForestClassifier()
-    model.fit(X, y_labels)
-
-    prediction = model.predict([feature])
-
-    st.subheader("猫の気持ち")
-    st.success(prediction[0])
+    y, sr = librosa.load(wav_path, sr=None)
