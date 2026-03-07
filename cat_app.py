b1709309edacbae509831adbd645393706cbd1ed
しましま <j.d.i.k.4110@gmail.com>
2026-03-08 01:25:54 +0900

    Add files via upload

--- cat_app.py
+++ cat_app.py
@@ -1,6 +1,39 @@
 import streamlit as st
-import sounddevice as sd
-from scipy.io.wavfile import write
+import streamlit as st
+import librosa
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("🐈 猫翻訳AI")
+
+uploaded_file = st.file_uploader("猫の鳴き声をアップロードしてください", type=["wav"])
+
+if uploaded_file is not None:
+
+    y, sr = librosa.load(uploaded_file)
+
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    feature = np.mean(mfcc.T, axis=0)
+
+    X = [
+        feature,
+        feature * 0.9,
+        feature * 1.1
+    ]
+
+    y_labels = [
+        "甘え",
+        "ご飯要求",
+        "怒り"
+    ]
+
+    model = RandomForestClassifier()
+    model.fit(X, y_labels)
+
+    prediction = model.predict([feature])
+
+    st.subheader("猫の気持ち")
+    st.success(prediction[0])
 import librosa
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
